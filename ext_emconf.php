c1f19ecf109e88787da36f55747b3417a9904564
Florian Langer <florian.langer@in2code.de>
2020-02-11 16:56:16 +0100

    [TASK] Remove 'cms' dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
 			'php' => '7.00.00-7.99.99',
 			'typo3' => '9.5.0-9.99.99',
 		),
